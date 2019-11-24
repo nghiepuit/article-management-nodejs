3e97d8f394476c1a71b2ec77fd3c7c797ef12dcd
nghiepuit <nghiepuit@gmail.com>
2019-11-24 18:55:53 +0700

    [master] update

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var http = require('http'),
-    path = require('path'),
-    methods = require('methods'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    cors = require('cors'),
-    passport = require('passport'),
-    errorhandler = require('errorhandler'),
-    mongoose = require('mongoose');
+var http = require("http"),
+    path = require("path"),
+    methods = require("methods"),
+    express = require("express"),
+    bodyParser = require("body-parser"),
+    session = require("express-session"),
+    cors = require("cors"),
+    passport = require("passport"),
+    errorhandler = require("errorhandler"),
+    mongoose = require("mongoose");
 
-var isProduction = process.env.NODE_ENV === 'production';
+var isProduction = process.env.NODE_ENV === "production";
 
 // Create global app object
 var app = express();
@@ -17,38 +17,45 @@ var app = express();
 app.use(cors());
 
 // Normal express config defaults
-app.use(require('morgan')('dev'));
+app.use(require("morgan")("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(require('method-override')());
-app.use(express.static(__dirname + '/public'));
+app.use(require("method-override")());
+app.use(express.static(__dirname + "/public"));
 
-app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+app.use(
+    session({
+        secret: "conduit",
+        cookie: { maxAge: 60000 },
+        resave: false,
+        saveUninitialized: false
+    })
+);
 
 if (!isProduction) {
-  app.use(errorhandler());
+    app.use(errorhandler());
 }
 
-if(isProduction){
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
-}
+mongoose
+    .connect(
+        "mongodb://nghiepuit:Nghiep2014@ds249008.mlab.com:49008/article_management"
+    )
+    .then(() => console.log("mongodb connected"))
+    .catch(e => console.log(e));
 
-require('./models/User');
-require('./models/Article');
-require('./models/Comment');
-require('./config/passport');
+require("./models/User");
+require("./models/Article");
+require("./models/Comment");
+require("./config/passport");
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 /// error handlers
@@ -56,29 +63,33 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (!isProduction) {
-  app.use(function(err, req, res, next) {
-    console.log(err.stack);
+    app.use(function(err, req, res, next) {
+        console.log(err.stack);
 
-    res.status(err.status || 500);
+        res.status(err.status || 500);
 
-    res.json({'errors': {
-      message: err.message,
-      error: err
-    }});
-  });
+        res.json({
+            errors: {
+                message: err.message,
+                error: err
+            }
+        });
+    });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({'errors': {
-    message: err.message,
-    error: {}
-  }});
+    res.status(err.status || 500);
+    res.json({
+        errors: {
+            message: err.message,
+            error: {}
+        }
+    });
 });
 
 // finally, let's start our server...
-var server = app.listen( process.env.PORT || 3000, function(){
-  console.log('Listening on port ' + server.address().port);
+var server = app.listen(process.env.PORT || 4000, function() {
+    console.log("Listening on port " + server.address().port);
 });
